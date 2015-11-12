f1f87fdd76ec8df514c27ff23cee407a7ef94dbd
lexa107 <lexa107@gmail.com>
2015-11-12 18:54:16 +0300

    Added done and add comment buttons functionality

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'), 										# /taskmanager/
 	url(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail'),				# /taskmanager/10/
-	url(r'^(?P<task_id>[0-9]+)/comments/$', views.comments, name='comments') 	# /taskmanager/10/comments
+	url(r'^(?P<task_id>[0-9]+)/comments/$', views.comments, name='comments'), 	# /taskmanager/10/comments
+	url(r'^(?P<task_id>[0-9]+)/add_comment/$', views.add_comment, name='add_comment'),
+	url(r'^(?P<task_id>[0-9]+)/delete_comment/(?P<comment_id>[0-9]+)/$', views.delete_comment, name='delete_comment'),
+	url(r'^task_done/(?P<task_id>[0-9]+)/$', views.task_done, name='task_done'),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Task
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+from .models import Task, Comment
 
 # Create your views here.
 def index(request):
@@ -14,3 +17,24 @@ def detail(request, task_id):
 
 def comments(request, task_id):
 	return HttpResponse("Task comments page. Not implemented yet")
+
+def add_comment(request, task_id):
+	task = get_object_or_404(Task, pk=task_id)
+	content = request.POST['content']
+	comment = Comment()
+	comment.content = content
+	comment.task = task
+	comment.save()
+	return HttpResponseRedirect(reverse('detail', args=(task.id,)))
+
+def delete_comment(request, task_id, comment_id):
+	comment = get_object_or_404(Comment, pk=comment_id)
+	task = get_object_or_404(Task, pk=task_id)
+	comment.delete()
+	return HttpResponseRedirect(reverse('detail', args=(task.id,)))
+
+def task_done(request, task_id):
+	task = get_object_or_404(Task, pk=task_id)
+	task.done = not task.done
+	task.save()
+	return HttpResponseRedirect(reverse('index'))
